130cd8b1580eab63ede7ff1dc9c321777a834167
Dominik Schön <ds@grafiniert.de>
2021-01-01 14:06:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const BlueLinky = require('bluelinky');
 const bodyParser = require('body-parser');
-const config = require('./config.json');
+const config = require('/config/config.json');
 
 const app = express();
 app.use(bodyParser.json());
